97cc5e10f9480435531be24e71968dc3511bd10a
xchalarm <120165210+xchalarm@users.noreply.github.com>
2024-12-17 14:34:43 +0700

    Update lab9_1.cpp

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,11 +2,14 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+    double loan,rate,year ;
 	cout << "Enter initial loan: ";
+	cin  >> loan ;
 	cout << "Enter interest rate per year (%): ";
+	cin  >> rate ; 
 	cout << "Enter amount you can pay per year: ";
-
+    cin  >> year ;
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -20,14 +23,28 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
 	
+	int i = 1;
+	    do{
+	if(loan == 0){
+	    break;
+	}
+	    
+	cout << fixed << setprecision(2); 
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << loan * rate / 100 ;
+	cout << setw(13) << left << loan + (loan * rate / 100 );
+	if ((loan + (loan * rate / 100 )) < year)
+	{
+	    year = loan + (loan * rate / 100 );
+	}
+	cout << setw(13) << left << year;
+	cout << setw(13) << left << loan + (loan * rate / 100 ) - year;
+	cout << "\n";	
+	loan = loan + (loan * rate / 100 ) - year;
+	i++;
+	    }
+	while(loan>1);
 	return 0;
 }
